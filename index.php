ee5ae591e06a2e6f40d09a65941534aba93e7948
Ahmad <Ahmad.Khan@cusolutionsgroup.com>
2019-11-22 13:05:03 -0600

    update
    update

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?
-echo 'Hello World';
+
+
+<!DOCTYPE html>
+<html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js"></script></head>
+
+<body>
+HI
+<script src="test.js">
+</script>
+
+<?php
+echo 'Check the console';
 ?>
+</body>
+</html>
---
+++ vm_provision/provision-ubuntu-16.04.sh
@@ -0,0 +1,45 @@
+
+#!/usr/bin/env bash
+
+# Intended for Ubuntu 12.03 (Whatever)
+
+# Update Ubuntu
+apt-get update
+
+# Adjust timezone to be Detroit
+ln -sf /usr/share/zoneinfo/America/Detroit /etc/localtime
+
+# Apache
+echo "----- Provision: Installing apache..."
+apt-get install -y apache2 apache2-utils php7.0 libapache2-mod-php7.0
+
+
+echo "----- Provision: Setup /var/www to point to /vagrant ..."
+rm -rf /var/www
+ln -fs /vagrant /var/www
+
+# Apache / Virtual Host Setup
+echo "----- Provision: Install Host File..."
+
+cp /vagrant/vm_provision/hostfile /etc/apache2/sites-available/000-default.conf
+
+a2enmod rewrite
+
+a2enmod php7.0
+
+
+# Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
+# sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+# sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+# sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+#
+#
+# # Installing PHP and it's dependencies
+# sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+
+# Cleanup unneded packages
+apt-get -y autoremove
+
+# Restart Apache
+echo "----- Provision: Restarting Apache..."
+service apache2 restart
